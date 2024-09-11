05798049991552eaf2e6efce62fd3c88a045b9e1
theaviralsharma <beingaviralsharma@gmail.com>
2024-09-11 23:06:09 +0530

    DSA

--- Chapter-9 | Strings/AssignmentQuestions.java
+++ Chapter-9 | Strings/AssignmentQuestions.java
@@ -11,19 +11,19 @@ public class AssignmentQuestions {
         }
         System.out.println(count);
     }
-    public static boolean checkAnagrams(String str1 , String str2){
+    public static boolean checkAnagrams(String s , String t){
         // Step 1: Check if lengths are equal
-        if (str1.length() != str2.length()) {
+        if (s.length() != t.length()) {
             return false;
         }
 
         // Step 2: Convert strings to lowercase (if case-insensitive)
-        str1 = str1.toLowerCase();
-        str2 = str2.toLowerCase();
+        s = s.toLowerCase();
+        t = t.toLowerCase();
 
         // Step 3: Convert to character arrays and sort
-        char[] charArray1 = str1.toCharArray();
-        char[] charArray2 = str2.toCharArray();
+        char[] charArray1 = s.toCharArray();
+        char[] charArray2 = t.toCharArray();
         Arrays.sort(charArray1);
         Arrays.sort(charArray2);
 
@@ -34,9 +34,9 @@ public class AssignmentQuestions {
         Scanner sc = new Scanner(System.in);
         // String str = sc.nextLine();
         // checkLowerCaseVowels(str);
-        String str1 = "listen";
-        String str2 = "silent";
-        System.out.println(checkAnagrams(str1,str2));
+        String s = "listen";
+        String t = "silent";
+        System.out.println(checkAnagrams(s,t));
         sc.close();
     }
 }
